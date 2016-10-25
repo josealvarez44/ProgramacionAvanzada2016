18d2cdd53fe443b110c9e06d327a4b8947188cb0
jose_alvarez_cabrera <cachorritoalfa2013@gmail.com>
2016-10-25 12:15:03 -0600

    Added Assignation of Request To Server Resources
    Assign ram numbers to server, processing numbers, and  request in
    general.

---
+++ InfixPostfixPrefix/InfixPostfixPrefix/Conversions.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <conio.h>
+#include <ctype.h>
+#include "stack.h"
+
+using namespace std;
+class CConversions
+{
+public:
+	CConversions();
+	~CConversions();
+	
+	string InfixToPostFix(string infixString);
+
+
+
+};
+
---
+++ InfixPostfixPrefix/InfixPostfixPrefix/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	char* s = "Hola";
+	
+	cout << s << endl;
+	return 0;
+}
